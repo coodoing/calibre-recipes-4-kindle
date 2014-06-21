fa75a59ff38d396065142dd08205b7db90d05dca
root <root@ubuntu.(none)>
2014-06-21 17:26:52 +0800

    add the autorun.sh and some recipes

---
+++ BeautifulSoupTest.py
@@ -0,0 +1,25 @@
+#-*-coding=utf-8-*-
+
+import os,sys,re
+from BeautifulSoup import BeautifulSoup
+
+doc = ['<html><head><title>Page title</title></head>',
+       '<body><p id="firstpara" align="center">This is paragraph <b>one</b>.',
+       '<p id="secondpara" align="blah">This is paragraph <b>two</b>.',
+       '</html>']
+doc1 = ['<div id="list">\
+<dl>\
+<dt>outlier</dt>\
+<dd><a href="1.html" title="ch1">ch1</a></dd>\
+<dd><a href="2.html" title="ch2">ch2</a></dd>\
+<dd><a href="3.html" title="ch3">ch3</a></dd>\
+<dd><a href="4.html" title="ch4">ch4</a></dd>\
+<dd><a href="5.html" title="ch5">ch5</a></dd>\
+<dd><a href="6.html" title="ch6">ch6</a></dd>\
+</dl></div>\
+']
+soup = BeautifulSoup(''.join(doc1))
+print(soup.prettify())
+#div = soup.find('div', { 'id': 'list' })
+div = soup.findAll(attrs={'id' : 'list'})
+print(div) 
---
+++ autorun.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "BEGIN: CALIBRE-RECIPE CONVERTION"
+
+SCRIPT=`readlink -f $0`
+SCRIPT_PATH=`dirname $SCRIPT`
+PHPSCRIPT=`which php`
+CALIBRE_PATH=`which calibre`
+EBOOK_CONVERTER_PATH=`which ebook-convert`
+EBOOK_VIEWER_PATH=`which ebook-viewer`
+#pwd
+#$#表示传递给此Shell脚本的参数个数  
+if [ $# -eq 0 ]   
+then  
+ echo "Usage: $0 <convert.recipe> "   
+ exit 1  
+fi 
+
+if [ $1 ]
+then
+ FILENAME=`echo $1|cut -d '.' -f 1`
+ FILETYPE=`echo $1|cut -d '.' -f 2`
+ #echo $FILENAME
+ SOURCE=$FILENAME."mobi"
+ if [ "$FILETYPE"x = "recipe"x ]
+ then
+  ebook-convert $1 $SOURCE
+ else
+  echo "Filetype must be 'recipe' format"
+  exit 1
+ fi
+ NUM=`ls $SOURCE|wc -l` 
+ if [ $NUM -eq 1 ]
+ then
+  ebook-viewer $SOURCE
+  echo "END CONVERTION"
+ else
+  echo "Convert problem occurs" 
+  exit 1
+ fi 
+else
+ echo "DONE"
+ exit 1
+fi
