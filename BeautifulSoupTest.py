f135a16634fa8e0559a897ffc9636ebbec1b8595
coodoing <jairdxs@gmail.com>
2014-06-22 10:24:01 +0800

    commit

--- BeautifulSoupTest.py
+++ BeautifulSoupTest.py
@@ -8,16 +8,16 @@ doc = ['<html><head><title>Page title</title></head>',
        '<p id="secondpara" align="blah">This is paragraph <b>two</b>.',
        '</html>']
 doc1 = ['<div id="list">\
-<dl>\
-<dt>outlier</dt>\
-<dd><a href="1.html" title="ch1">ch1</a></dd>\
-<dd><a href="2.html" title="ch2">ch2</a></dd>\
-<dd><a href="3.html" title="ch3">ch3</a></dd>\
-<dd><a href="4.html" title="ch4">ch4</a></dd>\
-<dd><a href="5.html" title="ch5">ch5</a></dd>\
-<dd><a href="6.html" title="ch6">ch6</a></dd>\
-</dl></div>\
-']
+			<dl>\
+			<dt>outlier</dt>\
+			<dd><a href="1.html" title="ch1">ch1</a></dd>\
+			<dd><a href="2.html" title="ch2">ch2</a></dd>\
+			<dd><a href="3.html" title="ch3">ch3</a></dd>\
+			<dd><a href="4.html" title="ch4">ch4</a></dd>\
+			<dd><a href="5.html" title="ch5">ch5</a></dd>\
+			<dd><a href="6.html" title="ch6">ch6</a></dd>\
+			</dl></div>\
+		']
 soup = BeautifulSoup(''.join(doc1))
 print(soup.prettify())
 #div = soup.find('div', { 'id': 'list' })
